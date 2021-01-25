43511ac25461ae9f2d1b036d0a877f81677468b5
Gilibeth Vega <gilibethvega@gmail.com>
2021-01-25 20:12:13 -0300

    se agrega historial de mascota con peso y altura en vista show

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,29 @@ class Pet < ApplicationRecord
   def to_s
     name
   end
+
+  def number_visits
+    PetHistory.references(:pets).where(pet_id: id).count
+  end
+
+  def history_pet
+    @history = PetHistory.references(:pets).where(pet_id: id).pluck(:weight, :height)
+    n = @history.count
+    new_array = []
+    array = ['Peso y altura en cita N°']
+    n.times do |i|
+      n_text = [i+1]
+      new_array.push array[0]
+      new_array.push [i+1]
+      new_array.push ['=']
+      n.times do |j|
+        new_array.push @history[i][j]
+      end
+      new_array.push '/'
+    end
+    final_array = new_array.join(' ')
+    final_array
+  end
+  
 end
+
